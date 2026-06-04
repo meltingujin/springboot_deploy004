5aa3826ed5a696ce17a00eab34611ddc7e362afb
meltingujin <leeyj991122@naver.com>
2026-06-04 11:26:19 +0900

    feet: 2026.06.04 리눅스 서버에 배포

--- deploy004_springboot/src/main/java/com/min/edu/ctrl/HomeController.java
+++ deploy004_springboot/src/main/java/com/min/edu/ctrl/HomeController.java
@@ -8,6 +8,6 @@ public class HomeController {
 
 	@GetMapping("/")
 	public String getMethodName() {
-		return new String("SpringBoot Docker 배포");
+		return new String("SpringBoot Docker 배포 자동으로 서버에도 배포해줘 (●'◡'●)");
 	}
 }
